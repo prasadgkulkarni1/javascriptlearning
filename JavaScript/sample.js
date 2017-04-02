277b6505d16e5a02a2c008a9218482e0e9a31ce6
Prasad Ganapathrao Kulkarni <prasadgkulkarni1@gmail.com>
2017-04-02 12:30:40 +1000

    This example shows another way of creating object class (with an object itself- SettingsParser) and using Object.create method to instantiate another object of first object type(SettingsParser).

--- JavaScript/sample.js
+++ JavaScript/sample.js
@@ -1,5 +1,8 @@
 
 //Define the class SettingsParser to parse the input JSON string into an object
+/* This method is using function constructor to define a class of objects (SettingsParser), then using this class to
+instantiate an object at runtime
+
 function SettingsParser(jsonString, settingsJson){
   this.settingsJsonString = jsonString;
   this.settings = settingsJson;
@@ -13,6 +16,37 @@ SettingsParser.prototype = {
   },
   printJson: function(){
       alert(this.settings.SETTINGS_RELEASE);
+  }
+}*/
+
+/**
+This method is using object definition (with its properties and methods), and using this as template for new object creation
+using this object's prototype
+*/
+
+var SettingsParser = {
+  settingsJsonString:null,
+  settings:null,
+
+  setSettingsJsonString:function(jsonString){
+    this.settingsJsonString = jsonString;
+  },
+
+  getSettingsJsonString:function(){
+    return this.settingsJsonString;
+  },
+
+  parseJsonString: function(){
+      alert(this.settingsJsonString)
+      this.settings = JSON.parse(this.settingsJsonString);
+  },
+
+  printJson: function(){
+      alert(this.settings.SETTINGS_RELEASE);
+  },
+
+  getSettings: function(){
+    return this.settings;
   }
 }
 
@@ -32,9 +66,19 @@ function readSingleFile(e) {
 
 //Displays the contents of the file- essentially calls the settings parser and prints the parsed object
 function displayContents(contents) {
+  /**
+  This is using function constructor to instantiate settings object
   var settings = new SettingsParser(contents, null);
+  */
+
+  /** This is using existing object SettingsParser to create another object of its type */
+  var settings = Object.create(SettingsParser);
+  settings.setSettingsJsonString(contents);
   settings.parseJsonString();
   settings.printJson();
+
+  alert("The prototype of settings object is SettingsParser:" + (Object.getPrototypeOf(settings) === SettingsParser))
+  alert("The prototype of SettingsParser object is Object's prototype:" + (Object.getPrototypeOf(SettingsParser) === Object.prototype))
 }
 
 //Reads the file-input HTML element and calls the parser once a file is uploaded.
